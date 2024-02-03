f4c5dd186a2a8714d4116dd9412aa049580982ee
di-saldana <dsl42@alu.ua.es>
2024-02-03 22:22:31 +0100

    Added: Búsqueda/Filtrado de platos

--- Restaurante/Platos/PlatosViewController.swift
+++ Restaurante/Platos/PlatosViewController.swift
@@ -3,16 +3,22 @@
 import UIKit
 import CoreData
 
-class PlatosViewController: UIViewController, UITableViewDataSource, NSFetchedResultsControllerDelegate, PlatoTableViewCellDelegate  {
+class PlatosViewController: UIViewController, UITableViewDataSource, NSFetchedResultsControllerDelegate, UISearchResultsUpdating, PlatoTableViewCellDelegate  {
+
     var frc : NSFetchedResultsController<Plato>!
     
     @IBOutlet weak var tabla: UITableView!
     
+    var searchController : UISearchController!
+//  let searchController = UISearchController(searchResultsController: nil)
+    let throttler = Throttler(minimumDelay: 0.5)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabla.dataSource = self
-        //TODO: crear un NSFetchedResultsController
         
+        //TODO: crear un NSFetchedResultsController
         let miDelegate = UIApplication.shared.delegate! as! AppDelegate
         let miContexto = miDelegate.persistentContainer.viewContext
         
@@ -23,18 +29,19 @@ class PlatosViewController: UIViewController, UITableViewDataSource, NSFetchedRe
 
         try! self.frc.performFetch()
         
-        if let resultados = frc.fetchedObjects {
-            print("Hay \(resultados.count) platos")
-            for plato in resultados {
-                print (plato.nombre!)
-            }
-        }
-        
         self.frc.delegate = self;
+        
+        self.searchController = UISearchController(searchResultsController: nil)
+        self.searchController.searchResultsUpdater = self
+        //Configuramos el search controller
+        self.searchController.obscuresBackgroundDuringPresentation = false
+        self.searchController.searchBar.placeholder = "Buscar texto"
+        //Lo añadimos a la tabla
+        self.searchController.searchBar.sizeToFit()
+        self.tabla.tableHeaderView = searchController.searchBar
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //TODO devolver el número de filas en la sección
         return self.frc.sections![section].numberOfObjects
     }
     
@@ -84,5 +91,31 @@ class PlatosViewController: UIViewController, UITableViewDataSource, NSFetchedRe
         navigationController?.pushViewController(vc, animated: true)
         
     }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        throttler.throttle {
+            let textoBuscado = searchController.searchBar.text!
+            
+            //Aquí iría tu código de búsqueda
+            
+            NSFetchedResultsController<NSFetchRequestResult>.deleteCache(withName:"Plato")
+            
+            let miDelegate = UIApplication.shared.delegate! as! AppDelegate
+            let miContexto = miDelegate.persistentContainer.viewContext
 
+            let consulta = NSFetchRequest<Plato>(entityName: "Plato")
+            let sortDescriptors = [NSSortDescriptor(key: "tipo", ascending: false)]
+            consulta.sortDescriptors = sortDescriptors
+
+            if !textoBuscado.isEmpty {
+                consulta.predicate = NSPredicate(format: "nombre CONTAINS[cd] %@", textoBuscado)
+            }
+
+            self.frc = NSFetchedResultsController<Plato>(fetchRequest: consulta, managedObjectContext: miContexto, sectionNameKeyPath: "tipo", cacheName: nil)
+
+            try! self.frc.performFetch()
+            self.tabla.reloadData()
+        }
+    }
+    
 }
